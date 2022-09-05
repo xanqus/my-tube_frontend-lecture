2c074e00eee131ef61cac7b651960b354591dbe8
xanqus <sbk98031816@gmail.com>
2022-09-05 12:44:07 +0900

    react router Link 컴포넌트

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div className="flex gap-2">
+        <span>
+          <Link to="/"> App</Link>
+        </span>
+        <span>
+          <Link to="/home"> Home</Link>
+        </span>
+        <span>
+          <Link to="/about"> About</Link>
+        </span>
+      </div>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
